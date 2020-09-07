396a9708292614b03f28433e2e5cfb94ac1f3bd0
ChenQShmily <chenqianshmily@126.com>
2020-09-07 22:03:01 +0800

    choose_teacher choose single teacher

--- frontend/src/components/student/choose_teacher.vue
+++ frontend/src/components/student/choose_teacher.vue
@@ -3,26 +3,27 @@
     <el-container>
       <el-main style="width:100%">
         <div>
-          <el-table :data="tables" style="width: 100%">
-            <el-table-column type="expand">
-              <template slot-scope="props">
-                <el-form label-position="left">
-                  <el-form-item label="姓名">
-                    <span>{{props.row.teacher_name}}</span>
-                  </el-form-item>
-                  <el-form-item label="研究所">
-                    <span>{{props.row.teacher_insititute}}</span>
-                  </el-form-item>
-                  <el-form-item label="个人主页">
-                    <span>{{props.row.teacher_info}}</span>
-                  </el-form-item>
-                </el-form>
+          <el-table 
+            :data = "tables"
+            ref = "multipleTable"
+            tooltip-effect="dark"
+            style="width:100%"
+            highlight-current-row >
+            <el-table-column width="45px">
+              <template scope="scope">
+                <el-radio v-model="radio" :label="scope.$index" @change="changeTeacher">{{''}}</el-radio>
               </template>
             </el-table-column>
-            <el-table-column label="姓名" prop="teacher_name"></el-table-column>
-            <el-table-column label="研究所" prop="teacher_institute"></el-table-column>
-            <el-table-column label="个人主页" prop="teacher_info"></el-table-column>
-
+            <el-table-column label="序号" style="width:5%" type="index"></el-table-column>
+            <template v-for='(col) in tableData'>
+              <el-table-column
+                :show-overflow-tooltip="true"
+                :prop="col.dataItem"
+                :label="col.dataName"
+                :key="col.dataItem"
+                style="width:90%">
+              </el-table-column>
+            </template>
           </el-table>
           <el-button type="primary" @click="commit">提交</el-button>
         </div>
@@ -35,7 +36,19 @@
 export default {
   data () {
     return {
-      tables: []
+      radio:'',
+      tables: [],
+      tableData:[{
+        dataItem: 'teacher_name',
+        dataName: '教师姓名'
+      },{
+        dataItem: 'teacher_institute',
+        dataName: '研究所'
+      },
+      {
+        dataItem: 'teacher_info',
+        dataName: '个人主页'
+      }]
     }
   },
   methods: {
@@ -48,12 +61,17 @@ export default {
     },
     commit () {
       var selectData = this.$refs.multipleTable.selection
-      console.log(selectData)
+      console.log(this.radio)
       this.$http
         .post('student_center/choose_teacher.', selectData)
         .then(result => {
           console.log(result.body)
         })
+    },
+    changeTeacher(row) {
+        console.log(row)
+        console.log(this.tables[row].teacher_name)
+        //this.radio = this.tables[row]
     }
   },
   created () {
