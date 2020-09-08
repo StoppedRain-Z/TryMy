4d8b424c38dd6949be60da227f30ac9fc0e9238b
ChenQShmily <chenqianshmily@126.com>
2020-09-08 19:54:22 +0800

    choose_queue

--- frontend/src/components/student/choose_queue.vue
+++ frontend/src/components/student/choose_queue.vue
@@ -2,8 +2,61 @@
   <div>
     <el-container>
       <el-main>
-        choose_queue
+        <el-main style="width:100%">
+        <div>
+          <el-table 
+            :data = "tables"
+            ref = "multipleTable"
+            tooltip-effect="dark"
+            style="width:100%"
+            highlight-current-row >
+            <el-table-column label="序号" style="width:5%" type="index"></el-table-column>
+            <template v-for='(col) in tableData'>
+              <el-table-column
+                :show-overflow-tooltip="true"
+                :prop="col.dataItem"
+                :label="col.dataName"
+                :key="col.dataItem"
+                style="width:90%">
+              </el-table-column>
+            </template>
+          </el-table>
+        </div>
+      </el-main>
       </el-main>
     </el-container>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      tables:[],
+      tableData:[{
+        dataItem: 'name',
+        dataName: '教师姓名'
+      },{
+        dataItem: 'teacher_choice',
+        dataName: '教师反馈'
+      }]
+    }
+  },
+  methods: {
+    getData() {
+      this.$http
+        .get('student_center/choose_queue')
+        .then(result => {
+          this.tables = result.body
+          
+        })
+      
+    }
+  },
+  created () {
+    this.getData()
+  }
+}
+
+</script>
+
